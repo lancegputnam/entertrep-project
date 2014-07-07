d772e9a8cfdf05635e7ebfe07d2e4ec2d6a83bf3
Lance Putnam <lancegputnam@gmail.com>
2014-07-07 11:36:53 -0400

    login view added

--- app/scripts/views.js
+++ app/scripts/views.js
@@ -8,17 +8,17 @@
 ////// Home View /////////                           
 var HomeView = Parse.View.extend ({
 
-	className: 'home-view'
+	className: 'home-view';
 
-	homeTemplate: _.template($".home-template"().text)
+	homeTemplate: _.template($".home-template"().text()),
 
 	events: {
-		"click .signup-button" : "signUp"
+		"click .signup-button" : "signUp",
 	}
 
 	initialize: function () {
 		$('.main-container').append(this.el);
-		this.render
+		this.render;
 	}
 
 	render: function () {
@@ -39,12 +39,12 @@ var HomeView = Parse.View.extend ({
 //////// Sign Up View /////////
 var SignUpView = Parse.View.extend ({
 
-	className: 'signup-view', 
+	className: 'signup-view'; 
 
-	signupTemplate: _.template($('.signup-template').text())
+	signupTemplate: _.template($('.signup-template').text()),
 
 	events: {
-		"click .register-button" : "register"
+		"click .register-button" : "register",
 	}
 
 	initialize: function() {
@@ -52,7 +52,70 @@ var SignUpView = Parse.View.extend ({
 		this.render
 	}
 
-	register: function() {
-		router.navigate('#/register', )
+	render: function () {
+		var renderedTemplate = this.signupTemplate
+		this.$el.html((renderedTemplate))
+		return this;
 	}
+
+	register: function() {
+		router.navigate('#/register', {trigger: true})
+	}
+
+	var user = new Parse.User();
+		user.set("username", "my username" , $(".add-user-name"));
+		user.set("password", "my pass", $(".add-password"));
+		user.set("email", "email@example.com", $(".add-email"));
+		user.set("first name", "my first name", $(".add-first-name"));
+		user.set("last name", "my last name", $(".add-last-name"));
+		user.set("zipcode", "digits", $(".add-zip-code"));
+		 
+		user.signUp(null, {
+		  success: function(user) {
+		    // Hooray! Let them use the app now.
+		  },
+		  error: function(user, error) {
+		    // Show the error message somewhere and let the user try again.
+		    alert("Error: " + error.code + "Please ensure login information is correct" + error.message);
+  		  }
+	});
 })
+
+/////// Login View.... NOT IN HTML YET! ////////// 
+var LoginView = Parse.View.extend ({
+
+	className: 'login-view'; 
+
+	loginTemplate: _.template($('.login-template').text()),
+
+	events: {
+		"click .login-button" : "login",
+	}
+
+	initialize: function() {
+		$('.main-container').append(this.el);
+		this.render
+	}
+
+	render: function () {
+		var renderedTemplate = this.loginTemplate
+		this.$el.html((renderedTemplate))
+		return this;
+	}
+
+	login: function () {
+		Parse.User.logIn($(".enter-username").val(), $(".enter-password").val(), {
+		  success: function(user) {
+		    $(".enter-username").val();
+			$(".enter-password").val();
+			router.navigate("#/")
+		  },
+		  error: function(user, error) {
+		    router.navigate("#/login")
+		  }
+		});
+	}
+)}
+
+///// still need user and and vendor views //////////
+//////// what about a search view //////////
